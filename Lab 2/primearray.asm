0b210b3a50a051906beda14fd4593cc94dbaaf47
TannerFindlay <tannerf978@gmail.com>
2024-10-08 16:16:01 -0700

    Update primearray.asm

--- Lab 2/primearray.asm
+++ Lab 2/primearray.asm
@@ -3,7 +3,7 @@
 //
 		.data
 		.align 4
-primearray:			.space 80			//Initializes pirme array to have 20 4-bit spaces (to hold prime numbers)
+primearray:			.space 80			//Initializes prime array to have 20 4-bit spaces (to hold prime numbers)
 
 
 
@@ -29,64 +29,74 @@ ComputePrimes:
 // Insert your code to compute the first 20 prime numbers here.
 //
 
+//r1 is i
+//r2 is n
+//r3 is prime
+//r4 is limit
+//r5 is j
+//r6 is the address of primearray
+//r7 is the quotients
+//r0 is the remainders
+
+
 main:
-		nop
-		ldr		r6, =primearray			//Loads r1 and r2 with the addresses of x and y, and then the values contained in those addresses
-		mov		r1, #3
+			nop
+		ldr		r6, =primearray		//load the address of primearray into r6
+		mov		r1, #3				//move #3 into r2, and #0 into r3
 		mov		r2, #0
 
-loop1:
-		cmp 	r2, #20
+loop1:						//BIG MAIN LOOP
+		cmp 	r2, #20				//if r2(n) >= #20, go to done
 		bge 	done
-		mov 	r3, #1
+		mov 	r3, #1				//move #1 into r3, #0 into r7, r1 into r0, and #2 into r5
 		mov 	r7, #0
 		mov 	r0, r1
 		mov 	r5, #2
 
-loop2:
-		cmp 	r0, #2
+loop2:						//Divide i by 2 to calculate limit
+		cmp 	r0, #2				//if r0 < 2, go to loop3
 		blt 	loop3
-		sub 	r0, #2
-		add 	r7, #1
-		b 		loop2
+		sub 	r0, #2				//subtract #2 from r0, keep in r0
+		add 	r7, #1				//add #1 to r7, keep in r7
+		b 		loop2				//repeat loop 2
 
-loop3:
-		mov r4, r7
-		cmp r5, r4
-		bge if2
-		mov r7, #0
-		mov r0, r1
+loop3:						//Going through each number >2 and <limit to see if i is divisible by any of them
+		mov 	r4, r7				//move r7 into r4 (the value for limit (quotient of loop2) into the 'limit' register)
+		cmp 	r5, r4				//if r5 >= r4, go to if2
+		bge 	if2
+		mov 	r7, #0
+		mov 	r0, r1
 
 loop4:
-		cmp r0, r5 //r0 is remainder, r5 is j
-		blt if1
-		sub r0, r5
-		add r7, #1
-		b 	loop4
+		cmp 	r0, r5 				//r0 is remainder, r5 is j
+		blt 	if1
+		sub 	r0, r5
+		add 	r7, #1
+		b 		loop4
 
 if1:
-		cmp r0, #0
-		bne else1
-		mov r3, #0
-		add r5, #1
-		b 	loop3
+		cmp		r0, #0
+		bne 	else1
+		mov 	r3, #0
+		add 	r5, #1
+		b 		loop3
 
 else1:
-		add r5, #1
-		b	loop3
+		add 	r5, #1
+		b		loop3
 
 if2:
-		cmp r3, #1
-		bne else2
-		str r1, [r6]
-		add r6, #4
-		add r1, #2
-		add r2, #1
-		b	loop1
+		cmp 	r3, #1
+		bne 	else2
+		str 	r1, [r6]
+		add 	r6, #4
+		add 	r1, #2
+		add 	r2, #1
+		b		loop1
 
 else2:
-		add r1, #2
-		b loop1
+		add 	r1, #2
+		b 		loop1
 
 
 //
