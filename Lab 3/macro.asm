8b5f08b3198e63c5957de962c5ab675aacc37a07
TannerFindlay <tannerf978@gmail.com>
2024-10-22 16:03:08 -0700

    Update macro.asm

--- Lab 3/macro.asm
+++ Lab 3/macro.asm
@@ -2,24 +2,24 @@
 	mov		r0, #1			// Initialize counter
 loop_begin\@:
 	cmp 	r0, \size		// If r0 is greater than size, end the loop
-	bgt 	loop_end
-	str 	r0, \destination		// Store r0 in the address
+	bgt 	loop_end\@
+	str 	r0, [\destination]		// Store r0 in the address
 	add 	\destination, #4		// Increment the address
 	add 	r0, #1			// Increment r0 by 1
-	b	loop_begin		// Repeat the loop
+	b	loop_begin\@		// Repeat the loop
 loop_end\@:
 	.endm
 
-.data
+	.data
 	.align 4
-	myArray: .space 600
+myArray: 	.space 600
 
 
 	.text
 	.global main
 
 main:
-	ldr	r2, =my_Array		// Load the address of myArray into r2
+	ldr	r2, =myArray		// Load the address of myArray into r2
 	mov	r3, #150		// Move #150 into r3
 	memorygenerate r2,r3		// Insert the macro memory generate with the parameters r2 and r3
 
